b50e5bac5bce47ba95489b15cf42847b598fe498
s1f10220277 <s1F102202774@iniad.org>
2024-06-17 02:18:54 +0900

    today's commit

--- 04-3/hit_and_blow.py
+++ 04-3/hit_and_blow.py
@@ -1,4 +1,12 @@
 # 関数名:hit(a,b)
 # リストa,bの対するヒットの個数を返す関数
 
-def hit(a,b
+def hit(a,b):
+   count=0
+   for i in range(0,4):
+    if a[i]==b[i]:
+      count+=1
+    return count
+   
+print("(1234, 4321) =>", hit([1,2,3,4],[4,3,2,1]),"hit")
+print("(1234, 1432) =>", hit([1,2,3,4],[1,4,3,2]),"hit")
