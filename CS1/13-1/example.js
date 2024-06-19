9f2c06a884933c28b586b966a13a9c38e799c2c1
s1f10220277 <s1F102202774@iniad.org>
2024-06-19 13:08:28 +0900

    today's commit!

---
+++ CS1/13-1/example.js
@@ -0,0 +1,56 @@
+// Python
+def area_of_circle(r):
+  return (r**2)*3.14
+
+  print('円の面積は'+area_of_circle(10)+'です')
+
+
+// JavaScript
+function area_of_circle(r){
+    return (r**2)*3.14159;
+}
+
+console.log('円の面積は'+area_of_circle(10)+'です')
+
+
+// Python
+ph = 8.0
+
+if ph<7.0:
+ print("酸性です")
+elif ph== 7.0:
+ print('中性です')
+else:
+ print('アルカリ性です')
+
+
+// JavaScript
+let ph= 8.0;
+
+if(ph<7.0){
+    console.log('酸性です');
+} else if (ph== 7.0){
+    console.log('中性です');
+} else{
+    console.log('アルカリ性です')
+}
+
+// Python
+i = 1
+sum=0
+
+while i <=10:
+ sum+=i
+ i+=1
+
+print(sum)
+
+// JavaScript
+let i = 1;
+let sum =0;
+
+while (i<= 10){
+    sum+=i;
+    i+=1;
+}
+console.log(sum);
